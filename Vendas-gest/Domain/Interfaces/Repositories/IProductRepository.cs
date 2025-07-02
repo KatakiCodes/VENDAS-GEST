1ed40a46ecbe0f8228c7d8acfed53aa60dd07a29
Nelson Dos Santos <katakicode001@gmail.com>
2025-07-02 11:51:09 +0100

    feat: Criando interface dos repositorios (user, product)

---
+++ Vendas-gest/Domain/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Repositories
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetAll(Product user);
+        User GetById(Product product, Guid id);
+        void Create(Product product);
+        void Update(Product product);
+    }
+}
---
+++ Vendas-gest/Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+namespace Domain.Repositories
+{
+    interface IUserRepository
+    {
+        IEnumerable<User> GetAll(User user);
+        User GetById(User user, Guid id);
+        void Create(User user);
+        void Update(User user);
+    }
+}
