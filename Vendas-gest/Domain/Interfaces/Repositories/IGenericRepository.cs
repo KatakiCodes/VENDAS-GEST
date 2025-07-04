939c3f056e53526930d15516bc60b3ff7b16b328
Nelson Dos Santos <katakicode001@gmail.com>
2025-07-04 11:45:37 +0100

    feat: Adicionando repositório gerêrico (IGenericRepository)

---
+++ Vendas-gest/Domain/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    interface IGenericRepository<T> where T : Entity
+    {
+        T GetById(Guid id);
+        void Create(T entity);
+        void Update(T entity);
+    }
+}
