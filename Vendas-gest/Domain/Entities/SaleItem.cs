34120af7565d8e14f8217a7b8217b6930158b0f5
Nelson Dos Santos <katakicode001@gmail.com>
2025-07-04 11:36:20 +0100

    feat: Adicionando Entidade, Testes de entidade e validadores (SaleItem)

---
+++ Vendas-gest/Domain/Entities/SaleItem.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Validators;
+using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+
+namespace Domain.Entities
+{
+    public class SaleItem : Entity, IValidatable
+    {
+        public SaleItem(Cart cart, Product product)
+        {
+            Cart = cart;
+            Product = product;
+            Quantity = 0;
+            _saleItemValidator = new SaleItemValidator(this);
+        }
+
+        public Cart Cart { get; private set; }
+        public Product Product { get; private set; }
+        public int Quantity { get; private set; }
+
+        private readonly SaleItemValidator _saleItemValidator;
+
+        public void AddQuantity(int quantity)
+        {
+            Product.SubtractStockQuantity(quantity);
+            if (Product.Valid is false)
+                this.AddNotifications(Product.Notifications);
+            else
+                Quantity += quantity;
+        }
+        public void SubtractQuantity(int quantity)
+        {
+            if (Product.Valid is false)
+                this.AddNotifications(Product.Notifications);
+            else if(quantity <= 0 || quantity > Quantity)
+                this.AddNotification(new Notification($"{quantity}","Não foi possível subtrair esta quantidade!"));
+            else
+            {
+                Product.AddStockQuantity(quantity);
+                Quantity -= quantity;
+            }
+        }
+
+
+        public void Validate()
+        {
+            _saleItemValidator.Validate();
+        }
+    }
+}
---
+++ Vendas-gest/Vendas-Gest.Tests/Entities.Tests/SaleItemTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vendas_Gest.Tests.Entities.Tests
+{
+    [TestClass]
+    public class SaleItemTests
+    {
+        private readonly Cart _cart;
+        private readonly Product _product;
+        private readonly SaleItem _validSaleItem;
+        private readonly SaleItem _invalidSaleItem;
+
+        public SaleItemTests()
+        {
+            _cart = new Cart();
+            _product = new Product("product1","prodDesc",1200,true);
+            _validSaleItem = new SaleItem(_cart, _product);
+            _invalidSaleItem = new SaleItem(_cart, null);
+            _product.AddStockQuantity(10);
+        }
+
+        [TestMethod]
+        public void Dado_um_item_invalido_o_mesmo_deve_retornar_erro()
+        {
+            _invalidSaleItem.Validate();
+            Assert.AreEqual(true, _invalidSaleItem.Invalid);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_o_mesmo_deve_ser_criado_com_sucesso()
+        {
+            _validSaleItem.Validate();
+            Assert.AreEqual(true, _validSaleItem.Valid);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_nao_deve_ser_incrementado_quantidade_menor_ou_igual_a_zero()
+        {
+            _validSaleItem.AddQuantity(-20);
+            Assert.AreEqual(false, _validSaleItem.Valid);
+            Assert.AreEqual(0, _validSaleItem.Quantity);
+            Assert.AreEqual(10, _validSaleItem.Product.StockQuantity);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_nao_deve_ser_incrementado_quantidade_maior_que_o_estoque_do_produto()
+        {
+            _validSaleItem.AddQuantity(120);
+            Assert.AreEqual(false, _validSaleItem.Valid);
+            Assert.AreEqual(0, _validSaleItem.Quantity);
+            Assert.AreEqual(10, _validSaleItem.Product.StockQuantity);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_deve_ser_incrementado_3_na_sua_quantidade()
+        {
+            _validSaleItem.AddQuantity(3);
+            Assert.AreEqual(3, _validSaleItem.Quantity);
+            Assert.AreEqual(7, _validSaleItem.Product.StockQuantity);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_nao_deve_ser_decrementado_valores_abaixo_de_zero_na_sua_quantidade()
+        {
+            _validSaleItem.SubtractQuantity(-3);
+            Assert.AreEqual(false, _validSaleItem.Valid);
+            Assert.AreEqual(0, _validSaleItem.Quantity);
+            Assert.AreEqual(10, _validSaleItem.Product.StockQuantity);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_nao_deve_ser_decrementado_valores_acima_sua_quantidade_existente()
+        {
+            _validSaleItem.AddQuantity(6);
+            _validSaleItem.SubtractQuantity(10);
+            Assert.AreEqual(false, _validSaleItem.Valid);
+            Assert.AreEqual(6, _validSaleItem.Quantity);
+            Assert.AreEqual(4, _validSaleItem.Product.StockQuantity);
+        }
+        [TestMethod]
+        public void Dado_um_item_valido_deve_ser_decrementado_3_na_sua_quantidade()
+        {
+            _validSaleItem.AddQuantity(6);
+            _validSaleItem.SubtractQuantity(3);
+            Assert.AreEqual(3, _validSaleItem.Quantity);
+            Assert.AreEqual(7, _validSaleItem.Product.StockQuantity);
+        }
+    }
+}
